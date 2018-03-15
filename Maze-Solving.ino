c478c3c940d2ba3a8e4feb9d7b03207db54c2609
Ihsan Soydemir <i.sydmr@gmail.com>
2018-03-16 00:13:54 +0300

    Add files via upload

---
+++ Maze-Solving.ino
@@ -0,0 +1,73 @@
+// # Editor     : Lauren from DFRobot
+// # Date       : 17.02.2012
+
+// # Product name: L298P Motor Shield
+// # Product SKU : DRI0009
+// # Version     : 1.0
+
+// # Description:
+// # The sketch for using the L298N motor shield
+
+// # Connection:
+// #		Directly connect the Motor shield to the Arduino controller 
+// #
+// #
+
+
+#define PWM1  6
+#define YON1  7
+#define PWM2  5                         
+#define YON2  4  
+#define ILERI HIGH
+#define GERI LOW
+                         
+int sensorpin =0;                 // analog pin used to connect the sharp sensor
+int onMesafe = 0;                 // variable to store the values from sensor(initially zero)
+
+
+void setup() 
+{ 
+    Serial.begin(9600);               // starts the serial monitor
+    pinMode(YON1, OUTPUT);   
+    pinMode(YON2, OUTPUT); 
+} 
+
+void loop() 
+{ 
+  int value;
+
+  onMesafe = analogRead(sensorpin);
+  int ref = 350;
+ 
+
+  int P = 2;
+  int hata = ref - onMesafe;
+
+  int cosku = P*hata;
+
+
+  int yon;
+
+  if(cosku <0){
+    yon = GERI;
+    cosku = -cosku;
+  }
+  else{
+    yon = ILERI;
+  }
+
+  if(cosku > 255) cosku = 255;
+  
+  digitalWrite(YON1, yon);
+  digitalWrite(YON2, yon);
+
+  analogWrite(PWM1, cosku);
+  analogWrite(PWM2, cosku);
+  
+
+ // sensor read 
+ // filtreleme
+  Serial.println(onMesafe);            // prints the value of the sensor to the serial monitor
+  delay(400);                    // wait for this much time before printing next value  
+}
+
